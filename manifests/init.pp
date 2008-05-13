98cae5c7c4d2aa877086ea2465eea9789deaaf51
Marcel Haerry <haerry@puzzle.ch>
2008-05-13 15:20:44 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# openldap module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "openldap": }
+class openldap {}
